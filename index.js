5e2086a984818101b824986a7cc59da89584ba86
IvanR96 <irios96@hotmail.com>
2025-11-04 17:01:34 -0800

    database connected

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "devops1996",
+  port: 5432,
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,9 +30,23 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
 
-app.post("/login", async (req, res) => {});
+  const result = db.query(
+    "INSERT INTO users (email, password) VALUES ($1, $2)",
+    [email, password]
+  );
+  console.log(result);
+  res.render("secrets.ejs");
+});
+
+app.post("/login", async (req, res) => {
+  let email = req.body.username;
+  let password = req.body.password;
+  
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
